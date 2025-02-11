c69f60ca2e4d289cf3086d6de4382dd7eabd62cb
Ahmad-46 <mubbaranauman@gmail.com>
2025-02-11 07:25:33 -0700

    e

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let mixed_Array = ["orange",50, ["mango","strawberry",4], 32];
-document.getElementById("Array").innerHTML = mixed_Array[2][1];
- 
+
+
+document.getElementById("Array").innerHTML = mixed_Array[2][0];
+
 let hobbies = ["painting","sketching","Gaming","gardening"];
 
 document.getElementById("join").innerHTML = hobbies.join(",");
@@ -11,3 +13,11 @@ document.getElementById("pop1").innerHTML = hobbies;
 hobbies.pop();
 
 document.getElementById("pop2").innerHTML = hobbies;
+
+hobbies.pop();
+
+document.getElementById("pop3").innerHTML = hobbies;
+
+hobbies.pop();
+
+document.getElementById("pop4").innerHTML = hobbies;
